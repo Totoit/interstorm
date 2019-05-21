a8d858088f10d7a1b9dd78aab1cca22aea55d99e
Tae P <bioheart.kalel@gmail.com>
2019-05-21 15:31:23 +0700

    restart project

--- interstorm_wheel/models.py
+++ interstorm_wheel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class test_db (models.Model):
+    class Meta:
+        db_table = "test_db"
+
+    name = models.CharField(max_length=255, default='-')
+    detail = models.TextField(max_length=255, default = '-')
+
+    def __str__(self):
+        return self.name
